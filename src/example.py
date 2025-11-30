64fe58e79140a2b4953f3ff17fbea2bc4a41f347
ktdtkpohhdj <annikakot_anuta@icloud.com>
2025-11-30 15:39:32 +0200

    Create example.py

---
+++ src/example.py
@@ -0,0 +1,34 @@
+class Example():
+    """
+    Creates example code with comments.
+
+    This documentation should be shown in example page.
+
+    If this doesn't shown there is some error I need to handle
+
+
+    """
+
+    def __init__():
+        """
+        This a small code do nothing
+        """
+
+
+    def generate_parameters():
+        """
+        And this do nothing, but documentation still should appear
+        """
+
+
+    def get_next_samples():
+        """
+        Gets the next batch of parameter combinations.
+
+        """
+
+
+
+    def register_future():
+        """ Doesn't matter for random sampler TODO: Probably? """
+        return None
