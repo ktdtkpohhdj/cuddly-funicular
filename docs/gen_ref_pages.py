34037fa61878abb575c9993b150ae835fe6be444
ktdtkpohhdj <annikakot_anuta@icloud.com>
2026-02-26 15:46:51 +0200

    Update gen_ref_pages.py

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -7,20 +7,19 @@ import sys
 SRC_PATH = Path("src")
 DOCS_PATH = Path("")  # генерируем прямо в docs/
 
-# Добавляем src в sys.path, чтобы импорты работали
+# Добавляем src в sys.path, чтобы пакеты, которые есть, можно было импортировать
 sys.path.insert(0, str(SRC_PATH.resolve()))
 
 for path in SRC_PATH.rglob("*.py"):
     if path.name == "__init__.py":
         continue
 
-    # Модуль относительно src
     module_path = path.relative_to(SRC_PATH).with_suffix("")
     module_name = ".".join(module_path.parts)
     doc_path = DOCS_PATH / module_path.with_suffix(".md")
     full_doc_path = Path("docs") / doc_path
 
-    # Не перезаписываем существующие ручные md
+    # Сохраняем существующие ручные md
     if full_doc_path.exists():
         continue
 
@@ -40,6 +39,6 @@ for path in SRC_PATH.rglob("*.py"):
             if spec is not None:
                 f.write(f"::: {module_name}\n")
             else:
-                f.write(f"_Module could not be imported_\n")
+                f.write("_Module could not be imported_\n")
         except ModuleNotFoundError:
-            f.write(f"_Module could not be imported_\n")
+            f.write("_Module could not be imported_\n")
